04a3a46cc3a15a2ac80d70879c8507e35b95cf41
Leandro Macedo <leandro.dmacedo@jsl.com.br>
2021-04-27 16:28:37 -0300

    Invertendo chamada das rotas para não haver conflito. Close #7

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/', require('./v1/routes-site'));
+// Rotas especificas precisam vir antes das rotas genericas,
+// pois algumas funcionalidades (middlewares, por exemplo) pode
+// acabar funcionando em rotas indevidas.
+
+// Rodas da V1 da API
 router.use('/api/v1', require('./v1/routes-api'));
+
+// Rotas do Site
+router.use('/', require('./v1/routes-site'));
 
 module.exports = router;
