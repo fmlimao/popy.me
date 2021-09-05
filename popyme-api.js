fd704a2407532a1d4885f7efc696bf7e7de4b7f8
Leandro Macedo <fmlimao@gmail.com>
2021-09-05 17:23:33 -0300

    Adicionando documentacao

--- popyme-api.js
+++ popyme-api.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use(express.static('./public'))
+app.use(express.static('./public'))
 
 // Rotas
 app.use(require('./src/routes'))
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const path = require('path')
 const router = express.Router()
+
+router.get('/doc', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/swagger/index.html'))
+})
+
+router.get('/doc.yaml', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../docs/fmlimao-PopyMe-1.0.0-resolved.yaml'))
+})
 
 router.get('/', require('./controllers/version/index'))
 
